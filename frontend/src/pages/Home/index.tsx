59e7d2620b890d1f30b2c7d0126bbd1415c163e4
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-09-11 14:59:59 -0300

    frontend - Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,31 @@
-const Home = () => {
+import Footer from 'components/Footer'
+import NavBar from 'components/Navbar'
+import { Link } from 'react-router-dom'
+
+function Home() {
   return (
-    <div>
-      <h1>Página home</h1>
-    </div>
+    <>
+      <NavBar />
+
+      <div className='container'>
+        <div className='jumbotron'>
+          <h1 className='display-4'>DJVendas</h1>
+          <p className='lead'>
+            Analise o desempenho das suas vendas por diferentes perspectivas!
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className='btn btn-primary btn-lg' to='/dashboard'>
+            Acessar o Dashboard
+          </Link>
+        </div>
+      </div>
+
+      <Footer />
+    </>
   )
 }
 
